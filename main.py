bed2d904fc4fd681019752887e2e315bb6430832
CIKAYA <115714689+CIKAYA@users.noreply.github.com>
2024-06-02 16:06:10 +0700

    update ke 2

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI(
-    title="Objek Wisata",
-    description="API untuk mengelola data objek wisata",
+    title="MC Tour Guide",
+    description="API TOUR GUIDE",
     docs_url="/",  # Ubah docs_url menjadi "/"
 )
 
@@ -15,77 +15,36 @@ async def read_root():
     return {"Data":"Successful"}
 
 # Model untuk Data Wisata
-class Wisata(BaseModel):
-    id_wisata: str
-    nama_objek: str
-    nama_daerah: str
-    kategori: str
-    alamat: str
-    kontak: str
-    harga_tiket: int
+class Tourguide(BaseModel):
+    id_guider: str
+    nama_guider: str
+    profile: str
+    no_hp: str
+    fee: int
+    history: int
+    feedback: int
+    bank: str
+    status_ketersediaan: str
 
 # Dummy data untuk wisata
-data_wisata = [
-    {"id_wisata": "OP01", "nama_objek": "Orchid Forest Cikole", "nama_daerah": "Bandung", "kategori": "Wisata Alam, Wisata Keluarga, Wisata Edukasi", "alamat": "Jl. Tangkuban Perahu Raya No.80E, Cikole, Lembang, Kabupaten Bandung Barat, Jawa Barat 40391, Indonesia", "kontak": "02282325888", "harga_tiket": 40000},
-    {"id_wisata": "OP02", "nama_objek": "Taman Impian Jaya Ancol", "nama_daerah": "Jakarta", "kategori": "Wisata Hiburan, Wisata Keluarga, Wisata Alam, Wisata Edukasi", "alamat": "Jl. Lodan Timur No.7, Ancol, Pademangan, Kota Jakarta Utara, Daerah Khusus Ibukota Jakarta 14430, Indonesia", "kontak": "02129222222", "harga_tiket": 25000},
-    {"id_wisata": "OP03", "nama_objek": "Candi Borobudur", "nama_daerah": "Yogyakarta", "kategori": "Wisata Budaya, Wisata Sejarah, Wisata Religi", "alamat": "Jl. Badrawati No.1, Borobudur, Magelang, Jawa Tengah 56553, Indonesia", "kontak": "0293788210", "harga_tiket": 50000},
-    {"id_wisata": "OP04", "nama_objek": "Uluwatu Temple", "nama_daerah": "Bali", "kategori": "Wisata Budaya, Wisata Religi, Wisata Alam", "alamat": "Pecatu, Kec. Kuta Selatan, Kabupaten Badung, Bali", "kontak": "0361915078", "harga_tiket": 50000},
-    {"id_wisata": "OP05", "nama_objek": "Surabaya North Quay", "nama_daerah": "Surabaya", "kategori": "Wisata Hiburan, Wisata Keluarga, Wisata Kuliner", "alamat": "Jalan Perak Timur, Perak Utara, Pabean Cantian, Kota Surabaya, Jawa Timur 60161", "kontak": "081336101290", "harga_tiket": 30000}
+data_tourguide = [
+    {"id_guider": "111", "nama_guider": "Chadkowi", "profile": "Seorang pemandu wisata berpengalaman dengan kecintaan pada petualangan alam yang menawarkan pengalaman mendaki gunung dan menjelajahi hutan yang luas.", "no_hp": "911", "fee": 200000, "history": 54, "feedback": 10, "bank": "GARIS", "status_ketersediaan": "Bisa"},
+    {"id_guider": "112", "nama_guider": "Prabroro", "profile": " Seorang pemandu wisata yang ahli dalam memperkenalkan budaya lokal dan sejarah di setiap destinasi.", "no_hp": "119", "fee": 400000, "history": 56, "feedback": 10, "bank": "LAUT BANK", "status_ketersediaan": "Bisa"},
+    {"id_guider": "113", "nama_guider": "Anisa", "profile": "Seorang pemandu wisata yang mengutamakan pengalaman mewah dan layanan berkualitas tinggi, memastikan setiap perjalanan menjadi pengalaman yang istimewa", "no_hp": "919", "fee": 500000, "history": 47, "feedback": 10, "bank": "BANG JAGO", "status_ketersediaan": "Bisa"},
+    {"id_guider": "114", "nama_guider": "Janggar", "profile": "Seorang pemandu wisata yang ramah keluarga dengan keahlian dalam merencanakan liburan yang menyenangkan bagi seluruh anggota keluarga.", "no_hp": "119", "fee": 450000, "history": 100, "feedback": 9, "bank": "MANDIRI", "status_ketersediaan": "Bisa"},
+    {"id_guider": "115", "nama_guider": "Mahfud DM", "profile": "mampu menjelaskan seluk beluk tempat-tempat yang dikunjungi saat perjalanan wisata.", "no_hp": "991", "fee": 300000, "history": 48, "feedback": 9, "bank": "BINI", "status_ketersediaan": "Bisa"}
 ]
 
 # Endpoint untuk menambahkan data wisata
-@app.post("/wisata")
-def tambah_wisata(wisata: Wisata):
-    data_wisata.append(wisata.dict())
+@app.post("/tourguide")
+def tambah_tourguide(tourguide: Tourguide):
+    data_tourguide.append(tourguide.dict())
     return {"message": "Data wisata berhasil ditambahkan."}
 
 # Endpoint untuk mendapatkan data wisata
-@app.get("/wisata", response_model=List[Wisata])
-def get_wisata():
-    return data_wisata
-
-# Fungsi untuk mengambil data pajak dari web hosting lain
-def get_data_pajak_from_web():
-    url = "https://example.com/api/pajak"  # Ganti dengan URL yang sebenarnya
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise HTTPException(status_code=response.status_code, detail="Gagal mengambil data PAJAK dari web hosting.")
-
-# Model untuk Data Pajak
-class Pajak(BaseModel):
-    id_pajak: int
-    jenis_pajak: str
-    tarif_pajak: float
-    besar_pajak: float
-
-# Endpoint untuk mendapatkan data pajak
-@app.get("/pajak", response_model=List[Pajak])
-def get_pajak():
-    data_pajak = get_data_pajak_from_web()
-    return data_pajak
-
-# Fungsi untuk mengambil data tourguide dari web hosting lain
-def get_data_tourGuide_from_web():
-    url = "https://example.com/api/pajak"  # Ganti dengan URL yang sebenarnya
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise HTTPException(status_code=response.status_code, detail="Gagal mengambil data TOUR GUIDE dari web hosting.")
-
-# Model untuk Data Tour Guide
-class TourGuide(BaseModel):
-    nama_guider: str
-    profile: str
-    fee: float
-
-# Endpoint untuk mendapatkan data Tour Guide
-@app.get("/tourGuide", response_model=List[TourGuide])
-def get_tourGuide():
-    data_tourGuide = get_data_tourGuide_from_web()
-    return data_tourGuide
+@app.get("/tourguide", response_model=List[Tourguide])
+def get_tourguide():
+    return data_tourguide
 
 # Fungsi untuk mengambil data asuransi dari web hosting lain
 def get_data_asuransi_from_web():
@@ -94,42 +53,85 @@ def get_data_asuransi_from_web():
     if response.status_code == 200:
         return response.json()
     else:
-        raise HTTPException(status_code=response.status_code, detail="Gagal mengambil data ASURANSI dari web hosting.")
+        raise HTTPException(status_code=response.status_code, detail="Gagal mengambil data Asuransi dari web hosting.")
 
 # Model untuk Data Asuransi
 class Asuransi(BaseModel):
-    id_asuransi: int
-    nama_wisata: str
-    nama_daerah: str
+    id_pajak: int
+    jenis_pajak: str
+    tarif_pajak: float
+    besar_pajak: float
 
-# Endpoint untuk mendapatkan data asuransi
+# Endpoint untuk mendapatkan data Asuransi
 @app.get("/asuransi", response_model=List[Asuransi])
 def get_asuransi():
     data_asuransi = get_data_asuransi_from_web()
     return data_asuransi
 
-# Fungsi untuk mengambil data hotel dari web hosting lain
-def get_data_hotel_from_web():
+# Fungsi untuk mengambil data tourguide dari web hosting lain
+def get_data_objekWisata_from_web():
     url = "https://example.com/api/pajak"  # Ganti dengan URL yang sebenarnya
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
     else:
-        raise HTTPException(status_code=response.status_code, detail="Gagal mengambil data HOTEL dari web hosting.")
+        raise HTTPException(status_code=response.status_code, detail="Gagal mengambil data OBJEK WISATA dari web hosting.")
 
-# Model untuk Data Hotel
-class Hotel(BaseModel):
-    id_room: int
-    room_number: int
-    room_type: str
-    rate: str
-    availability: int
+# Model untuk Data Tour Guide
+class ObjekWisata(BaseModel):
+    nama_guider: str
+    profile: str
+    fee: float
 
-# Endpoint untuk mendapatkan data hotel
-@app.get("/hotel", response_model=List[Hotel])
-def get_hotel():
-    data_hotel = get_data_hotel_from_web()
-    return data_hotel
+# Endpoint untuk mendapatkan data Tour Guide
+@app.get("/objekWisata", response_model=List[ObjekWisata])
+def get_objekWisata():
+    data_objekWisata = get_data_objekWisata_from_web()
+    return data_objekWisata
+
+# Fungsi untuk mengambil data asuransi dari web hosting lain
+def get_data_government_from_web():
+    url = "https://example.com/api/pajak"  # Ganti dengan URL yang sebenarnya
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Gagal mengambil data GOVERNMENT dari web hosting.")
+
+# Model untuk Data Asuransi
+class Government(BaseModel):
+    id_asuransi: int
+    nama_wisata: str
+    profile: str
+
+# dpoint untuk mendapatkan data asuransi
+@app.get("/government", response_model=List[Government])
+def get_government():
+    data_government = get_data_government_from_web()
+    return data_government
+
+# # Fungsi untuk mengambil data hotel dari web hosting lain
+# def get_data_hotel_from_web():
+#     url = "https://example.com/api/pajak"  # Ganti dengan URL yang sebenarnya
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         return response.json()
+#     else:
+#         raise HTTPException(status_code=response.status_code, detail="Gagal mengambil data HOTEL dari web hosting.")
+
+# # Model untuk Data Hotel
+# class Hotel(BaseModel):
+#     id_room: int
+#     room_number: int
+#     room_type: str
+#     rate: str
+#     availability: int
+
+# # Endpoint untuk mendapatkan data hotel
+# @app.get("/hotel", response_model=List[Hotel])
+# def get_hotel():
+#     data_hotel = get_data_hotel_from_web()
+#     return data_hotel
 
 # Fungsi untuk mengambil data bank dari web hosting lain
 def get_data_bank_from_web():
